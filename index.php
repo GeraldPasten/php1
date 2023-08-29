b0cf01b183432bad680be6db95fd7dbff07c0890
Gerald Pasten <gpasten@redhat.com>
2023-08-29 12:40:53 -0400

    Subiendo git

---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+ <head>
+  <title>PHP Deploy Test OCP</title>
+ </head>
+ <body>
+ <?php echo '<p>Hello BANORBRAS</p>'; ?> 
+ <img src="php-helloworld-master\logo.jpg" alt="Descripción de la imagen">
+
+ </body>
+</html>
