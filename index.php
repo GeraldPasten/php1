edc59569fde46d53ab4da8a67b4f74ce929b41c6
Gerald Pasten <gpasten@redhat.com>
2023-08-29 15:36:46 -0400

    logo

--- index.php
+++ index.php
@@ -6,15 +6,23 @@
  <?php echo '<p>HELLO BANOBRAS</p>'; ?> 
  <img src="./logo.jpg" alt="Descripción de la imagen">
  <button id="logButton">Enviar console log</button>
+ <button id="refreshButton">Refrescar página</button>
 
     <script>
         // Obtener una referencia al botón
         var logButton = document.getElementById("logButton");
-
+        var refreshButton = document.getElementById("refreshButton");
         // Agregar un evento click al botón
         logButton.addEventListener("click", function() {
             // Imprimir mensaje en la consola del navegador cuando se hace clic en el botón
-            error_log('Mensaje enviado desde el botón.');
+            console.log('Mensaje enviado desde el botón.');
+            
+        });
+
+         // Agregar un evento click al botón de refrescar
+         refreshButton.addEventListener("click", function() {
+            // Recargar la página
+            location.reload();
         });
     </script>
 
